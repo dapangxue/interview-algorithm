a5f11dc3cb10f090e7f4153c85c5210d633fef6a
Wu Xue <imwuxue@gmail.com>
2019-05-13 14:15:22 +0800

    update printZigZagBinaryTree

---
+++ src/interview/zuo/chapter3/ZigZagPrintBinaryTree.java
@@ -0,0 +1,65 @@
+package interview.zuo.chapter3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Create by IDEA
+ *
+ * @author wuxue
+ * @date 19-5-13
+ */
+public class ZigZagPrintBinaryTree {
+    static class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int value;
+
+        public TreeNode(int value) {
+            this.value = value;
+        }
+    }
+
+    public static List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root != null) {
+            Stack<TreeNode>[] stack = new Stack[2];
+            stack[0] = new Stack();
+            stack[1] = new Stack();
+            int fix = 0;
+            stack[fix].push(root);
+            while (!stack[0].isEmpty() || !stack[1].isEmpty()) {
+                List<Integer> list = new ArrayList<>();
+                if ((fix & 1) == 0) {
+                    while (!stack[0].isEmpty()) {
+                        root = stack[0].pop();
+                        if (root.left != null) {
+                            stack[1].push(root.left);
+                        }
+                        if (root.right != null) {
+                            stack[1].push(root.right);
+                        }
+                        list.add(root.value);
+                    }
+                } else {
+                    while (!stack[1].isEmpty()) {
+                        root = stack[1].pop();
+                        if (root.right != null) {
+                            stack[0].push(root.right);
+                        }
+                        if (root.left != null) {
+                            stack[0].push(root.left);
+                        }
+                        list.add(root.value);
+                    }
+                }
+                result.add(list);
+                System.out.println(result);
+                fix = fix == 0 ? 1 : 0;
+            }
+        }
+
+        return result;
+    }
+}
